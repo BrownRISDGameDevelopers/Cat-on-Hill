97dbd66f2739385bb0c0c3780f14c06df6ff4862
DarkShinyGiratina <williamsross20@gmail.com>
2023-11-04 13:50:13 -0400

    added neat assets

--- Assets/Scripts/RollingObstacle.cs
+++ Assets/Scripts/RollingObstacle.cs
@@ -3,39 +3,37 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class BouncingObstacle : ObstacleControl
-{
-    private Rigidbody2D rb;
-    public float yVel;
-    public float xVel;
-    public float spinSpeed;
-    public float maxHeight;
-    // Start is called before the first frame update
-    protected override void Start()
-    {
-      base.Start();
-        rb = gameObject.GetComponent<Rigidbody2D>();
-    }
+public class BouncingObstacle : ObstacleControl {
+  private Rigidbody2D rb;
+  public float yVel;
+  public float xVel;
+  public float spinSpeed;
+  public float maxHeight;
+  // Start is called before the first frame update
+  protected override void Start() {
+    base.Start();
+    rb = gameObject.GetComponent<Rigidbody2D>();
+  }
 
-    // Update is called once per frame
-    protected override void Update()
-    {
-        base.Update();
-        transform.Rotate(0,0, spinSpeed * Time.deltaTime);
-        transform.position += new Vector3(xVel * Time.deltaTime, -yVel * Time.deltaTime, 0);
-        if (transform.position.y > maxHeight) {
-            yVel *= -1;
-        }
+  // Update is called once per frame
+  protected override void Update() {
+    base.Update();
+    transform.Rotate(0, 0, spinSpeed * Time.deltaTime);
+    transform.position += new Vector3(xVel * Time.deltaTime, -yVel * Time.deltaTime, 0);
+    if (transform.position.y > maxHeight) {
+      yVel *= -1;
     }
-    protected override void OnTriggerEnter2D(Collider2D other) {
-        string otherTag = other.tag;
-        switch (otherTag) {
-            case "Floor":
-                yVel *= -1;
-            break;
-            case "Player":
-                state = States.DESTROYABLE;
-            break;
-        }
+  }
+  protected override void OnTriggerEnter2D(Collider2D other) {
+    string otherTag = other.tag;
+    Debug.Log("Collided " + otherTag);
+    switch (otherTag) {
+      case "Floor":
+        yVel *= -1;
+        break;
+      case "Player":
+        state = States.DESTROYABLE;
+        break;
     }
+  }
 }
