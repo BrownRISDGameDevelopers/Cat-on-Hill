9a4617e1a8891ec3bfcfdd71ee02c1f36d9c4a4b
Lucas Chan <lucas_chan@brown.edu>
2023-11-04 15:42:04 -0400

    Added spinning when hit obstacle

--- Assets/Scripts/ObstacleControl.cs
+++ Assets/Scripts/ObstacleControl.cs
@@ -14,11 +14,21 @@ public class ObstacleControl : MonoBehaviour {
   public float timerToChangeSprite;
   public Sprite deadSprite;
   public float beat;
+  private float deathxVel;
+  private float deathyVel;
+  private float deathSpinSpeed;
+  private float deathMaxHeight;
+  public ParticleSystem deathParticleSystem;
+
   // Start is called before the first frame update
   protected virtual void Start() {
     state = States.UNINTERACTABLE;
     // beat gives us the starting x position, offset b/c character starts at -7.04
     transform.position = new Vector3(beat - 3.04f, transform.position.y, transform.position.z);
+    deathxVel = -12.5f;
+    deathyVel = 12.5f;
+    deathSpinSpeed = 2250f;
+    deathMaxHeight = 4f;
   }
 
   // Update is called once per frame
@@ -27,11 +37,19 @@ public class ObstacleControl : MonoBehaviour {
       case States.DESTROYABLE:
         if (Input.GetKey("space")) {
           gameObject.GetComponent<SpriteRenderer>().sprite = deadSprite;
+          // var em = deathParticleSystem.emission;
+          // em.enabled = true;
+          // deathParticleSystem.Play();
           state = States.DYING;
         }
         break;
       case States.DYING:
         timerToChangeSprite -= Time.deltaTime;
+        transform.Rotate(0, 0, deathSpinSpeed * Time.deltaTime);
+        transform.position += new Vector3(deathxVel * Time.deltaTime, deathyVel * Time.deltaTime, 0);
+        if (transform.position.y >= deathMaxHeight) {
+          deathyVel *= -1;
+        }
         if (timerToChangeSprite <= 0) {
           state = States.DESPAWNING;
         }
