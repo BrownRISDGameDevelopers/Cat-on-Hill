0e51bc488b16fe33f880849f28a8a3c6d8e36b3f
Siddarth Sitaraman <siddarth_sitaraman@brown.edu>
2023-11-11 15:56:17 -0500

    scoring added

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public int overall_score;
+    public TextMeshProUGUI scoreText;
+    public float percentPerfectScore; //TODO
     // Start is called before the first frame update
     void Start()
     {
         overall_score = 0;
+        scoreText.text = "SCORE: " + overall_score.ToString();
+
     }
 
     // Update is called once per frame
@@ -19,5 +24,6 @@ public class ScoreManager : MonoBehaviour
 
     public void UpdateScore(int score) {
         overall_score += score;
+        scoreText.text = "SCORE: " + overall_score.ToString();
     }
 }
