6a4e1a20ee29686de5851c8cb24a2a198898c8c2
DarkShinyGiratina <williamsross20@gmail.com>
2023-12-02 15:04:45 -0500

    tutorial now ends on time

---
+++ Assets/Scripts/FadeTransition.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeTransition : MonoBehaviour {
+
+  public Animator animator;
+  public float transitionTime = 0.5f;
+  // Start is called before the first frame update
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  public void MoveToScene(int sceneID) {
+    StartCoroutine(SceneTransition(sceneID));
+  }
+
+  IEnumerator SceneTransition(int sceneID) {
+    animator.SetTrigger("Start");
+
+    yield return new WaitForSeconds(transitionTime);
+
+    SceneManager.LoadScene(sceneID);
+  }
+}
