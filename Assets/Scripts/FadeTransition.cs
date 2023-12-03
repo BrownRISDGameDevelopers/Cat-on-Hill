9896f7e9615f804af5afbfb8a74ea868986ad667
Lucas Chan <lucas_chan@brown.edu>
2023-12-03 03:21:28 -0500

    Uh

--- Assets/Scripts/FadeTransition.cs
+++ Assets/Scripts/FadeTransition.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class FadeTransition : MonoBehaviour {
 
   public Animator animator;
-  public float transitionTime = 0.5f;
+  public float transitionTime;
 
   public AudioSource audioSource;
 
